f6eba307ad75e8fe81799c13134c73e142d443b8
Pawel Niechoda <pawel.niechoda@gmail.com>
2016-01-18 11:59:22 +0200

    add babel-polyfill

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+import 'babel-polyfill';
 import React from 'react';
 import ReactDom from 'react-dom';
 
 import App from './components/App.jsx';
 
+
 import './styles/style.styl';
+
 
 ReactDom.render(<App />, document.getElementById('app'));
