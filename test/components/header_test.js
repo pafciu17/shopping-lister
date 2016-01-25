7f59fadaa8d78d2a05839cd9c840155e196e6f52
Pawel Niechoda <pawel.niechoda@gmail.com>
2016-01-25 13:54:49 +0200

    rewrite header_test to use shallow rendering

--- test/components/header_test.js
+++ test/components/header_test.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import TestUtils from 'react-addons-test-utils';
-import ReactDom from 'react-dom';
+import { renderShallow } from '../utils';
 
 import Header from '../../src/components/Header';
 
-describe("Header", () => {
-  it("renders test component text", () => {
-    var title = 'test title'
-    var header = TestUtils.renderIntoDocument(<Header title={title}/>);
-    var domeNode = ReactDom.findDOMNode(header);
-    expect(domeNode.innerHTML).toContain(title);
+describe('Header', () => {
+  let renderedHeader;
+  const title = 'test title';
+
+  beforeAll(() => {
+    renderedHeader = renderShallow(<Header title={title} />);
+  });
+
+  it('has class: header', () => {
+    expect(renderedHeader.props.className).toBe('header');
+  });
+
+  it('as a first child contains span with set inner html and defined header__title class', () => {
+    const span = renderedHeader.props.children;
+
+    expect(span.type).toBe('span');
+    expect(span.props.className).toBe('header__title');
+    expect(span.props.children).toBe(title);
   });
 });
